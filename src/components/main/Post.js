7378bf81178e5b4b643952d12cb73c469eb68f80
mugyeol <htpadkorik@naver.com>
2022-12-23 16:06:26 +0900

    feat: test

--- src/components/main/Post.js
+++ src/components/main/Post.js
@@ -5,6 +5,7 @@ import FlexColumn from "../../ui/layout/FlexColumn";
 const Post = () => {
   return (
     <Fragment>
+      <div className="test"></div>
     <Card pd="1rem" border="1px solid var(--ig-elevated-separator)">
       <FlexColumn>mock data</FlexColumn>
     </Card>
@@ -14,7 +15,7 @@ const Post = () => {
 
 export default Post;
 const Fragment = styled.div`
-
+  .test{}
 
 `
 // 현재의 FlexColum, FlexRow, Card에 props추가 해야하는 경우 
--- src/components/main/PostList.js
+++ src/components/main/PostList.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Post from "./Post";
+import styled from 'styled-components';
+import { StFlexColumn } from "../../ui/layout/FlexColumn";
 import FlexColumn from "../../ui/layout/FlexColumn";
 
 const PostList = () => {
   const arr = new Array(10).fill("mock data");
   console.log(arr)
   return (
-    <FlexColumn>
+    <FlexColumn style={{gap:'1rem'}}>
       {arr.map((mock, index) => (
         <Post key={index} />
       ))}
@@ -16,3 +18,7 @@ const PostList = () => {
 
 export default PostList;
 
+const MyColumn = styled(StFlexColumn)`
+    /* gap: 1rem; */
+
+`
